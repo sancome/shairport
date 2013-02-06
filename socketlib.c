eb41bb1f7c5a2c8a5f09f8ec6d3d18d5cc74f9fd
Evan Chau <sancome@163.com>
2013-02-06 09:28:58 +0800

    //tFamily = AF_INET6;

--- socketlib.c
+++ socketlib.c
@@ -159,7 +159,7 @@ int setupListenServer(struct addrinfo **pAddrInfo, int pPort)
     int tFamily = AF_INET;
     #ifdef AF_INET6
     //printf("Listening on IPv6 Socket\n");
-    tFamily = AF_INET6;
+    //tFamily = AF_INET6;
     #else
     //printf("Listening on IPv4 Socket");
     #endif
